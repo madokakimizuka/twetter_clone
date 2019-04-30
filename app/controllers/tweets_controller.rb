0bf34f6e6bd7d1b126acbf568d6398548fe56a2b
君塚まどか <kimitaro0415@gmail.com>
2019-04-30 15:40:02 +0900

    tweet投稿時にエラーメッセージ表示機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,9 +8,14 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
-    redirect_to new_tweet_path
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "ブログを作成しました！"
+    else
+      render 'new'
+    end
   end
+
 
   private
 
