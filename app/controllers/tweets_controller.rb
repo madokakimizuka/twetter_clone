f97b35c74d388066c45ae7d8887ab97e8575c3f7
君塚まどか <kimitaro0415@gmail.com>
2019-04-30 17:03:04 +0900

    編集機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update]
   def index
     @tweets = Tweet.all
   end
@@ -16,10 +17,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "編集しました！"
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
